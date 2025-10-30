671c59b3505c565cfb601652610650b9764e78d6
kana <kana>
2025-10-30 13:18:05 +0500

    parsecommandline

--- data/reader.go
+++ data/reader.go
@@ -1,48 +1,47 @@
 package data
 
 import (
+	"errors"
 	"flag"
-	"fmt"
 	"os"
 )
 
-type AppArgs struct {
-	Command string
-	Inputs  []string
-	Brands  string
-	Issuers string
-	Brand   string
-	Issuer  string
-	Stdin   bool
-	Pick    bool
+type CommandLineInput struct {
+	Command   string
+	Args      []string
+	UseStdin  bool
+	Pick      bool
+	Brands    string
+	Issuers   string
+	BrandName string
+	Issuer    string
 }
 
-func ParseArgs() *AppArgs {
+func ParseCommandLine(args []string) (*CommandLineInput, error) {
 	if len(os.Args) < 2 {
-		fmt.Printf("Usage: %s [OPTIONS]\n", os.Args[0])
-		os.Exit(1)
+		return nil, errors.New("No command provided")
 	}
+	cmd := args[0]
 
-	app := &AppArgs{}
-	app.Command = os.Args[1]
-	
-	fs := flag.NewFlagSet(app.Command, flag.ExitOnError)
-	fs.BoolVar(&app.Stdin, "stdin", false, "read input from stdin")
-	fs.BoolVar(&app.Pick, "pick", false, "pick random entry (for generate)")
-	fs.StringVar(&app.Brands, "brands", "", "path to brands.txt")
-	fs.StringVar(&app.Issuers, "issuers", "", "path to issuers.txt")
-	fs.StringVar(&app.Brand, "brand", "", "brand name (for issue)")
-	fs.StringVar(&app.Issuer, "issuer", "", "issuer name (for issue)")
+	fs := flag.NewFlagSet(cmd, flag.ContinueOnError)
+	useStdin := fs.Bool("stdin", false, "read input from stdin")
+	pick := fs.Bool("pick", false, "randomly pick one number")
+	brands := fs.String("brands", "", "path to brands file")
+	issuers := fs.String("issuers", "", "path to issuers file")
+	brand := fs.String("brand", "", "brand name for issue command")
+	issuer := fs.String("issuer", "", "issuer name for issue command")
 
-	if err := fs.Parse(os.Args[2:]); err != nil {
-		fmt.Fprintln(os.Stderr, "Error parsing arguments:", err)
-		os.Exit(1)
+	if err := fs.Parse(args[1:]); err != nil {
+		return nil, err
 	}
-
-	app.Inputs = fs.Args()
-	return app
+	return &CommandLineInput{
+		Command:   cmd,
+		Args:      fs.Args(),
+		UseStdin:  *useStdin,
+		Pick:      *pick,
+		Brands:    *brands,
+		Issuers:   *issuers,
+		BrandName: *brand,
+		Issuer:    *issuer,
+	}, nil
 }
-
-//func printUsage() {
-//
-//}
--- main.go
+++ main.go
@@ -3,9 +3,37 @@ package main
 import (
 	"creditcard/data"
 	"fmt"
+	"os"
 )
 
 func main() {
-	args := data.ParseArgs()
-	fmt.Println(args)
+	args := os.Args[1:]
+	input, err := data.ParseCommandLine(args)
+	if err != nil {
+		os.Exit(1)
+	}
+	fmt.Printf("%+v\n", input)
+
 }
+
+//func AlgorithmLuna(s string) bool {
+//	sum := 0
+//	double := false
+//
+//	for i := len(s) - 1; i >= 0; i-- {
+//		if s[i] < '0' || s[i] > '9' {
+//			return false
+//		}
+//		digit := int(s[i] - '0')
+//
+//		if double {
+//			digit = digit * 2
+//			if digit > 9 {
+//				digit = digit - 9
+//			}
+//		}
+//		sum = sum + digit
+//		double = !double
+//	}
+//	return sum%10 == 0
+//}
